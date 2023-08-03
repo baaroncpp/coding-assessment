cf97c06733f3074ee16e892dcd5e31616468c631
baaroncpp <baaronlubega1@gmail.com>
2023-08-03 12:49:49 +0300

    Add code-assessment to github

---
+++ src/test/java/com/interswitch/codingassessment/CodingAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interswitch.codingassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
